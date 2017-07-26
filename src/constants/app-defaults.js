5667ae2444c8e572df227cced5e097c4dcfa4e77
OmairAzam <omairr.azam@gmail.com>
2017-07-26 09:00:35 +0500

    facebooked constant converted to string.

--- src/constants/app-defaults.js
+++ src/constants/app-defaults.js
@@ -1,6 +1,6 @@
 const APP_DEFAULTS = {
   perPage: 5,
-  facebookAppId: 414367592293859,
+  facebookAppId: '414367592293859',
   googleAppId: '840317428406-scoi7rvq4ie2f48pru2lrlsmu0thoa2r.apps.googleusercontent.com'
 };
 
